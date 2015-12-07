f7560107bf191fedf3a80e9c4bf7f280776918f7
wgabsi88 <wgabsi88@gmail.com>
2015-12-07 21:54:49 +0100

    login_implementation

--- app/src/main/java/com/example/stefan/androidwebsockets/LoginActivity.java
+++ app/src/main/java/com/example/stefan/androidwebsockets/LoginActivity.java
@@ -1,11 +1,20 @@
 package com.example.stefan.androidwebsockets;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.Intent;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by waelgabsi on 26.11.15.
@@ -15,6 +24,7 @@ public class LoginActivity  extends AppCompatActivity {
     private EditText mUsernameView;
     private EditText mPasswordView;
     private String mUsername;
+    ProgressDialog prgDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +36,112 @@ public class LoginActivity  extends AppCompatActivity {
         Button signInButton = (Button) findViewById(R.id.login_button);
 
 
+        prgDialog = new ProgressDialog(this);
+        // Set Progress Dialog Text
+        prgDialog.setMessage("Please wait...");
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
+        
 
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                String username = mUsernameView.getText().toString().trim();
-                Intent activityChangeIntent = new Intent(LoginActivity.this, ProjekteActivity.class);
-                activityChangeIntent.putExtra("username", username);
-                // currentContext.startActivity(activityChangeIntent);
 
-                LoginActivity.this.startActivity(activityChangeIntent);
+    }
+
+    public void loginUser(View view){
+        // Get Email Edit View Value
+        String email = mUsernameView.getText().toString();
+        // Get Password Edit View Value
+        String password = mPasswordView.getText().toString();
+        // Instantiate Http Request Param Object
+        RequestParams params = new RequestParams();
+        // When Email Edit View and Password Edit View have values other than Null
+        if(Utility.isNotNull(email) && Utility.isNotNull(password)){
+            // When Email entered is Valid
+            if(Utility.validate(email)){
+                // Put Http parameter username with value of Email Edit View control
+                params.put("username", email);
+                // Put Http parameter password with value of Password Edit Value control
+                params.put("password", password);
+                // Invoke RESTful Web Service with Http parameters
+                invokeWS(params);
+            }
+            // When Email is invalid
+            else{
+                Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_LONG).show();
+            }
+        }
+        // When any of the Edit View control left blank
+        else{
+            Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    /**
+     * Method that performs RESTful webservice invocations
+     *
+     * @param params
+     */
+    public void invokeWS(RequestParams params){
+        // Show Progress Dialog
+        prgDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://alpha.taskql.com/rest/api/1/taskql/login",params ,new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getString("errorCode").equals("0")) {
+                        Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
+                        // Navigate to Home screen
+                        navigatetoHomeActivity();
+                    }
+                    // Else display error message
+                    else{
+                        //errorMsg.setText(obj.getString("error_msg"));
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
 
-
+    /**
+     * Method which navigates from Login Activity to Home Activity
+     */
+    public void navigatetoHomeActivity(){
+        Intent homeIntent = new Intent(getApplicationContext(),ProjekteActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
     }
 
 
