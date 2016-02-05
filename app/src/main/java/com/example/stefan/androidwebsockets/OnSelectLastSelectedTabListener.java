7c3daa2cb3806418153424ae41e2f66fbdc3ade7
Stefan Völkel <stefanvoelkel@web.de>
2016-02-05 15:02:29 +0100

    Added OnSelectLastSelectedTabListener.java

---
+++ app/src/main/java/com/example/stefan/androidwebsockets/OnSelectLastSelectedTabListener.java
@@ -0,0 +1,9 @@
+package com.example.stefan.androidwebsockets;
+
+/**
+ * Created by domar007 on 04.02.2016.
+ */
+public interface OnSelectLastSelectedTabListener {
+    public void selectLastSelectedTab();
+    public void selectLastSelectedTabText();
+}
